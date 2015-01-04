1af1c16e2a667e292dc9933f7f1a4b68142501ac
eneskemalergin <eneskemalergin@gmail.com>
2015-01-04 05:01:06 -0600

    file upgraded

--- Head First Python/Chapter 3/file.py
+++ Head First Python/Chapter 3/file.py
@@ -1,10 +1,31 @@
 # Imports "os" from the standart library
+
 # operating system...
 import os
 
 # it shows your current directory
 os.getcwd()
 
-#
-os.chdir("C:\\Users\\Pc-41\\Documents")
+# It changes the current directory to a directory which has our file that we want to examine
+os.chdir("C:\\Users\\Pc-41\\Desktop\\Python Examples\\Head First Python\\Chapter 3")
 
+
+file = open('sketch.txt') # This command opens the txt file now it is ready to be read
+file.close() # this function closes it.
+
+# Let's have fun with our file using Python
+data = open('sketch.txt')
+# name_of_variable.readline() reads the line and print
+print(data.readline(), end = '')
+# Same statement again reads the line after the first one, in this case is second line.
+print(data.readline(), end = '')
+
+
+# use the seek() method to return to the start of the file. and yes, you can use tell with Python's files, too
+data.seek()
+
+# Following code prints all the line in the file
+for each_line in data:
+    print(each_line, end = '')
+
+data.close()
