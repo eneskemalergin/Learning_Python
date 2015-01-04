2fa7570828e35ab9a3ccc8d3c8224f27cfb82613
eneskemalergin <eneskemalergin@gmail.com>
2015-01-04 00:30:20 -0600

    Chapter1.1

---
+++ Head First Python/Chapter 1/Chapter1.1.py
@@ -0,0 +1,30 @@
+# print() function is a built-in functions that shows your work in the screen.
+
+print("My name is Enes Kemal Ergin")
+
+''' Unlike other C based languages, which use { and } to delimit blocks, python
+    uses indentation instead. '''
+# By the way it indentation is very important try to get used to it.
+
+# Python IDLE allows you to put your code and shows the result of that code immediately
+
+
+# TAB key completes your built-in function that you started to type.
+
+# Alt-P recall the previous code
+# Alt-N move to the next code statement
+
+
+# We can arrange data as a list.
+
+# Creating list
+movies = ["The Holy Grail",
+          "The Life of Brian",
+          "The Meaning of Life"]
+# This is another way 
+movies = ["The Holy Grail", "The Life of Brian", "The Meaning of Life"]
+
+# The list adress starts from index 0
+# For example if we have 3 elements in the list
+# We will have 0, 1, 2 indexes
+print(movies[1])
