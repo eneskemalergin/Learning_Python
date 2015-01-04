c09e9de697cdd49d25cfbf5f2fe94e1d53ee1082
eneskemalergin <eneskemalergin@gmail.com>
2015-01-04 03:30:37 -0600

    module example

---
+++ Head First Python/Chapter 2/nester.py
@@ -0,0 +1,14 @@
+## The sets for Sharing our Code
+
+# Modules let you organize your code for optimal sharing.
+
+# The distribution utilities let you share your modules with the world.
+
+# Now this file called nester.py is sharable with the world, and called in terms of python; 'module.'
+def print_each(the_list):
+    for i in the_list:
+        if isinstance(each_item, list):
+            print_each(each_item)
+        else:
+            print(each_item)
+        
