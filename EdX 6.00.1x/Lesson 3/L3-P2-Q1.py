85d8e2d5550f7e1a3d3dd8e84fc0948ebdf79342
eneskemalergin <eneskemalergin@gmail.com>
2015-01-10 07:33:58 -0600

    Lesson 3 Problem 2 solutions

---
+++ EdX 6.00.1x/Lesson 3/L3-P2-Q1.py
@@ -0,0 +1,19 @@
+## L3 - P2 - Q1
+
+''' Convert the following into code that uses a while loop.
+
+print 2
+print 4
+print 6
+print 8
+print 10
+print Goodbye!
+
+'''
+
+num = 2
+while num <= 10:
+    if num % 2 == 0 :
+        print(num)
+    num += 1
+print("Goodbye!")
---
+++ EdX 6.00.1x/Lesson 3/L3-P2-Q2.py
@@ -0,0 +1,17 @@
+## L3 - P2 - Q2
+
+'''Convert the following into code that uses a while loop.
+
+print Hello!
+print 10
+print 8
+print 6
+print 4
+print 2
+
+'''
+num = 10
+print("Hello!")
+while num >= 2:
+    print(num)
+    num -= 2
