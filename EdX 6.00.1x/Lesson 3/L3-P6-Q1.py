4d2786a3dcb8585488e82dc3f6d61f75112dab90
eneskemalergin <eneskemalergin@gmail.com>
2015-01-10 08:59:32 -0600

    Lesson 3 Problems 6 solutions

---
+++ EdX 6.00.1x/Lesson 3/L3-P6-Q1.py
@@ -0,0 +1,9 @@
+## L3 - P6 - Q1
+
+iteration = 0
+count = 0
+while iteration < 5:
+    for letter in "hello, world":
+        count += 1
+    print("Iteration " + str(iteration) + "; count is: " + str(count))
+    iteration += 1 
---
+++ EdX 6.00.1x/Lesson 3/L3-P6-Q2.py
@@ -0,0 +1,9 @@
+## L3 - P6 - Q2
+
+iteration = 0
+while iteration < 5:
+    count = 0
+    for letter in "hello, world":
+        count += 1
+    print("Iteration " + str(iteration) + "; count is: " + str(count))
+    iteration += 1 
