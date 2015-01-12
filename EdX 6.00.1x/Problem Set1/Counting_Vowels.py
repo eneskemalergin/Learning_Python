c5c59161d0aa5b23db94a216b952f3721bad1bac
eneskemalergin <eneskemalergin@gmail.com>
2015-01-12 11:25:53 -0600

    Updated Counting_Vowels

--- EdX 6.00.1x/Problem Set1/Counting_Vowels.py
+++ EdX 6.00.1x/Problem Set1/Counting_Vowels.py
@@ -1,3 +1,4 @@
+# Counting Vowels
 # Author: Enes Kemal Ergin
 ''' Assume s is a string of lower case characters.
 
@@ -6,12 +7,6 @@ Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example,
 if s = 'azcbobobegghakl', your program should print:
 
 Number of vowels: 5
-
-For problems such as these, do not include raw_input statements or define 
-the variable s in any way. Our automated testing will provide a value of s 
-for you - so the code you submit in the following box should assume s is already
-defined. If you are confused by this instruction, please review L4 Problems 10
-and 11 before you begin this problem set.
 '''
 s = 'azcbobobegghakl'
 s_list = list(s) # Make a list with characters
