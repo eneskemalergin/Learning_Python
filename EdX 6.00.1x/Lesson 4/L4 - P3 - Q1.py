9aeefe34dd9c6741fe108a7ba582794af02805bf
eneskemalergin <eneskemalergin@gmail.com>
2015-01-13 12:49:30 -0600

    Lesson 4 Problem 3 solution

---
+++ EdX 6.00.1x/Lesson 4/L4 - P3 - Q1.py
@@ -0,0 +1,7 @@
+## L4 - P3 - Q1
+'''Write a Python function, square,
+that takes in one number and returns the square of that number.
+This function takes in one number and returns one number. 
+'''
+def square(x):
+    return(x * x)
