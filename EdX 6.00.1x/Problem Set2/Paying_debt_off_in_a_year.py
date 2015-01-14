6d2f44eb748a0a6bf53ead65a59dcda5af3390fd
eneskemalergin <eneskemalergin@gmail.com>
2015-01-14 03:05:50 -0600

    Updated Problem set 2 part 2: Paying_debt_off_in_a_year (still some bugs...)

--- EdX 6.00.1x/Problem Set2/Paying_debt_off_in_a_year.py
+++ EdX 6.00.1x/Problem Set2/Paying_debt_off_in_a_year.py
@@ -32,17 +32,38 @@ is found below:
     (Monthly interest rate x Monthly unpaid balance)
 '''
 
-balance = 3329
-annualInterestRate = 0.2
-count = 1
-total = 0
-prev_balance = balance
-while count <= 12:
+
+
+
+def paying_debt(balance, annualInterestRate):
+    monthly_payment = 10
     monthly_interest_rate = annualInterestRate / 12.0
-    minimum_monthly_payment = 10
-    monthly_unpaid_balance = prev_balance - minimum_monthly_payment
-    prev_balance = monthly_unpaid_balance + (monthly_interest_rate * monthly_unpaid_balance)
-    count += 1
-    minimum_monthly_payment += 10
-    
-print("Lowest Payment: " + str(round(prev_balance, 2)))
+    updated_balance = balance - 10
+
+    while updated_balance > 0:
+        monthly_payment += 10
+        updated_balance = balance
+        count = 0
+        while count < 12 and updated_balance >0:
+            interest = monthly_interest_rate * updated_balance
+            updated_balance -= monthly_payment
+            updated_balance += interest
+            count += 1
+
+    print("Lowest Payment: " + str(monthly_payment))
+
+print(paying_debt(3329, 0.2))
+print(paying_debt(4773, 0.2))   # Wrong
+print(paying_debt(3926, 0.2))   # Wrong
+print(paying_debt(839, 0.18))
+print(paying_debt(763, 0.25))
+print(paying_debt(536, 0.18))
+print(paying_debt(749, 0.2))
+print(paying_debt(3632, 0.2))
+print(paying_debt(3527, 0.04))  # Wrong
+print(paying_debt(3665, 0.04))
+print(paying_debt(4984, 0.04))
+print(paying_debt(4289, 0.04))
+print(paying_debt(3781, 0.15))  # Wrong
+print(paying_debt(3701, 0.15))  # Wrong
+print(paying_debt(4064, 0.04))
