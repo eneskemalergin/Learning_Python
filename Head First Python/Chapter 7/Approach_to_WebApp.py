8fc923e106a25e920086d8e57c8ccaa68828b8e2
eneskemalergin <eneskemalergin@gmail.com>
2015-01-14 13:32:34 -0600

    Added Approach_to_WebApp

---
+++ Head First Python/Chapter 7/Approach_to_WebApp.py
@@ -0,0 +1,43 @@
+# Approach to Web App
+
+## Previously we have done some work, but for now, we need to share it to everybody...
+
+## How we can share it to everybody to make them accessable
+
+## We can put our program on the Web, Web-based application
+
+
+## What are the requirements to make a webapp ?
+
+'''
+    - Nice friendly homepage?
+    - Reach and get things easily
+    - printing?
+
+Let's now hear Coach's request, what he wants...
+
+    - Friendly graphics
+
+    Welcome to Coach Kelly’s Website
+For now, all that you’ll find here is my
+athlete’s timing data.
+See you on the track!
+
+    when you clict the "timing data" it will take it to the list:
+
+Select an athlete from this list to work with:
+Sarah
+James
+Julie
+Mikey
+
+    chosing sarah will lead us to timing data of Sarah
+
+    
+    Timimg data for Sarah:
+            2.18
+            2.21
+            2.22
+    Home                Select another athlete
+
+'''
