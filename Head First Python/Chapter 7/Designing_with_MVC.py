c069d56905c39a78fa6d81e6ab648bede44c9afb
eneskemalergin <eneskemalergin@gmail.com>
2015-01-14 13:36:25 -0600

    Added Designing_with_MVC

---
+++ Head First Python/Chapter 7/Designing_with_MVC.py
@@ -0,0 +1,10 @@
+# Designing with MVC
+
+''' The best way to make an approach is using Model-View-Controller(MVC) '''
+
+# The Model         :  the code to store your webapp's data
+
+# The View          :  The code to format and display your webapp's user interface(s)
+
+# The controller    :  The code to glue your webapp together and provide its business logic
+
